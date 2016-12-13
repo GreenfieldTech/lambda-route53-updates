f7a610a006da0ec89fc2d15cfcf46a87b94f09bb
Oded Arbel <oded@geek.co.il>
2016-12-13 18:05:57 +0200

    don't run live tests from maven

--- src/test/java/net/gftc/aws/route53/TestRoute53.java
+++ src/test/java/net/gftc/aws/route53/TestRoute53.java
@@ -2,6 +2,8 @@ package net.gftc.aws.route53;
 
 import static org.junit.Assert.*;
 
+import java.util.Objects;
+
 import org.junit.Test;
 
 import com.amazonaws.services.route53.AmazonRoute53Client;
@@ -11,6 +13,7 @@ import com.amazonaws.services.route53.model.RRType;
 public class TestRoute53 {
 	
 	static String testDomain = System.getenv("TEST_DOMAIN");
+	static boolean disableLiveTests = Objects.nonNull(System.getenv("DISABLE_LIVE_TESTS"));
 
 	@Test
 	public void testAddA() {
@@ -18,7 +21,8 @@ public class TestRoute53 {
 		ChangeResourceRecordSetsRequest cr = Tools.getAndAddRecord(
 				"test." + testDomain, 
 				RRType.A, "5.6.7.8");
-		Tools.waitFor(r53.changeResourceRecordSets(cr));
+		if (!disableLiveTests)
+			Tools.waitFor(r53.changeResourceRecordSets(cr));
 		assertTrue(true);
 	}
 
@@ -38,11 +42,13 @@ public class TestRoute53 {
 		ChangeResourceRecordSetsRequest cr = Tools.getAndAddRecord(
 				"_sip._udp.test." + testDomain, 
 				RRType.SRV, "1 1 5060 test1." + testDomain);
-		Tools.waitFor(r53.changeResourceRecordSets(cr));
+		if (!disableLiveTests)
+			Tools.waitFor(r53.changeResourceRecordSets(cr));
 		ChangeResourceRecordSetsRequest cr2 = Tools.getAndAddRecord(
 				"_sip._udp.test." + testDomain, 
 				RRType.SRV, "1 1 5060 test2." + testDomain);
-		Tools.waitFor(r53.changeResourceRecordSets(cr2));
+		if (!disableLiveTests)
+			Tools.waitFor(r53.changeResourceRecordSets(cr2));
 		assertTrue(true);
 	}
 
@@ -52,7 +58,8 @@ public class TestRoute53 {
 		ChangeResourceRecordSetsRequest cr = Tools.getAndRemoveRecord(
 				"_sip._udp.test." + testDomain, 
 				RRType.SRV, "1 1 5060 test1." + testDomain);
-		Tools.waitFor(r53.changeResourceRecordSets(cr));
+		if (!disableLiveTests)
+			Tools.waitFor(r53.changeResourceRecordSets(cr));
 		assertTrue(true);
 	}
 
